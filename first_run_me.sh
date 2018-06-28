428f66a8f473d1cdce29e73f0278441b61635484
kakabomba <kakabomba@gmail.com>
2018-06-28 14:58:33 +0300

    Update first_run_me.sh

--- first_run_me.sh
+++ first_run_me.sh
@@ -3,7 +3,8 @@
 apt-get install git python-pip python-dev sudo
 pip install ansible==2.3.1.0
 
-usermod -aG sudo $(cat /etc/passwd | tail -n1 | grep 1000 | sed -e 's/:.*//g')
+sudo_user=$(cat /etc/passwd | tail -n1 | grep 1000 | sed -e 's/:.*//g')
+usermod -aG sudo "$sudo_user"
 
 git config --global user.email "kakabomba@gmail.com"
 git config --global user.name "Oles Zaburannyi"
@@ -11,4 +12,4 @@ git config --global push.default simple
 git clone https://github.com/kakabomba/ansible.git
 
 cd ansible
-ansible-playbook -i 'localhost,' --connection=local ./playbooks/common.yml
+ansible-playbook -i 'localhost,' --connection=local ./playbooks/common.yml --extra-vars "sudo_user=$sudo_user"
