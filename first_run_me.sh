a0e9471520550a10a6513ec79b013bb305ebec56
kakabomba <kakabomba@gmail.com>
2018-06-28 13:55:53 +0300

    Update first_run_me.sh

--- first_run_me.sh
+++ first_run_me.sh
@@ -2,7 +2,7 @@
 
 apt-get install git ansible
 
-usermod -aG sudo $(cat /etc/passwd/ | tail -n1 | grep 1000 | sed -e 's/:.*//g')
+usermod -aG sudo $(cat /etc/passwd | tail -n1 | grep 1000 | sed -e 's/:.*//g')
 
 git config --global user.email "kakabomba@gmail.com"
 git config --global user.name "Oles Zaburannyi"
