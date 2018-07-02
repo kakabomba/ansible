5235eccb8516ee09f3ee1024715e2bc67caa8053
kakabomba <kakabomba@gmail.com>
2018-07-02 11:32:39 +0300

    Update first_run_me.sh

--- first_run_me.sh
+++ first_run_me.sh
@@ -10,7 +10,6 @@ usermod -aG sudo "$sudo_user"
 git config --global user.email "kakabomba@gmail.com"
 git config --global user.name "Oles Zaburannyi"
 git config --global push.default simple
-git clone https://github.com/kakabomba/ansible.git
 
 cd ansible
-ansible-playbook -i 'localhost,' --connection=local ./playbooks/common.yml --extra-vars "sudo_user=$sudo_user sudo_user_group=$sudo_user_group"
+ansible-playbook -i 'localhost,' --connection=local ./playbooks/common.yml --extra-vars="sudo_user=$sudo_user sudo_user_group=$sudo_user_group"
